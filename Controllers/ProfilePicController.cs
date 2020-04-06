cd75e40caa34a42e33843280368df147f9b1502a
Ankita1405 <ankusharrma007.as@gmail.com>
2020-04-06 15:43:33 +0530

    Initial Commit

---
+++ Controllers/ProfilePicController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Practice6april.Models;
+
+namespace Practice6april.Controllers
+{
+    public class ProfilePicController : Controller
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly ILogger<ProfilePicController> _logger;
+
+        public ProfilePicController(ILogger<ProfilePicController> logger,
+            IWebHostEnvironment env)
+        {
+            _logger = logger;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string UploadFile(ProfilePic picupload)
+        {
+            string ext = System.IO.Path.GetExtension(picupload.FormFile.FileName);
+        
+
+
+
+
+            if (picupload.FormFile != null &&( ext==".jpeg" || ext==".jpg" || ext == ".gif" || ext == ".png"))
+            {
+                string filepath = $"{_env.WebRootPath}/images/{ picupload.FormFile.FileName}";
+
+
+                var stream = System.IO.File.Create(filepath);
+                picupload.FormFile.CopyTo(stream);
+            }
+            else
+            {
+                return "Only Images of type .jpg,.gif,.png or .jpeg is accepted";
+            }
+            return "PROFILE PIC UPLOAD SUCCESSFUL";
+
+        }
+    }
+}
---
+++ Models/ProfilePic.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practice6april.Models
+{
+    public class ProfilePic
+    {
+        public IFormFile FormFile{ get; set; }
+    }
+}
